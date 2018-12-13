b28db208ada099a7f25d848060107e986d52a00c
rlassche <you@example.com>
2018-12-13 10:50:20 +0100

    .

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
-import { Kinvey } from 'kinvey-angular2-sdk';
+import { Kinvey, User } from 'kinvey-angular2-sdk';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-signup',
@@ -8,77 +9,62 @@ import { Kinvey } from 'kinvey-angular2-sdk';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
-  user: {};
+  kinveyUser:Kinvey.User ;
   error: Kinvey.BaseError;
   username: string = ''
   password: string = ''
   firstname: string = ''
   lastname: string = ''
-  errorMessage:string
+  errorMessage: string
 
   constructor(private router: Router, private changeDetectorRef: ChangeDetectorRef) {
-    console.log('signup constructor')
-    this.user = new Kinvey.User({
-      _id: '',
-      //_acl: { /* ACL */ },
-      //_kmd: { /* Metadata */ },
-      //email: 'email'
-      username: '',
-      password: ''
-    });
+    console.log('signup constructor');
+    this.activeUser() ;
   }
 
+  activeUser() {
+    this.kinveyUser = Kinvey.User.getActiveUser();
+    console.log( "ActiveUser ...", this.kinveyUser)
+    if (this.kinveyUser) {
+      this.kinveyUser.me()
+        .then((activeUser: Kinvey.User) => {
+          console.log('then: activeUser', activeUser.username)
+          this.errorMessage = 'Cannot signup. User ' + activeUser.username + ' is already logged in!'
+        this.changeDetectorRef.detectChanges()
+        })
+        .catch((error: Kinvey.BaseError) => {
+          // ...
+          console.log('catch: error', error)
+          this.errorMessage = error.message;
+        this.changeDetectorRef.detectChanges()
+        });
+    } else {
+      console.log( 'activeuser else tak')
+    }
+  }
   ngOnInit() {
   }
   signup() {
-    console.log('this is signup.component: signup', this.user)
-    this.errorMessage = undefined;
+    console.log('this is signup.component: signup')
+    //this.errorMessage = undefined;
 
-    let promise = Kinvey.User.signup({
+    let promise: Promise<User> = Kinvey.User.signup({
       username: this.username,
       password: this.password,
       firstname: this.firstname,
       lastname: this.lastname
     });
 
-    this.router.navigate(['/kinvey']);
-
-    /*
-    Kinvey.User.signup(this.username, this.password)
-      .then(() => {  console.log( 'login: okay' ); this.router.navigate(['/'])} )
+    promise.then(
+      () => {
+        console.log('login: okay');
+        this.router.navigate(['/'])
+      })
       .catch((error: Kinvey.BaseError) => {
         this.error = error;
-        console.log( 'login: error is now', this.error)
+        console.log('signup: error is now', this.error)
 
         this.changeDetectorRef.detectChanges()
       });
-      */
-
-
-
-      /*
-    promise.then(() => {
-      console.log('promise then signup')
-      this.router.navigate(['/logout']);
-    }).catch((error: Kinvey.BaseError) => {
-      console.log('promise error signup', error.message)
-      // Update the view
-      console.log('error is now: ', error)
-      this.errorMessage = error.message
-      //this.changeDetectorRef.detectChanges()
-    });
-    */
-    /*
-    Kinvey.User.signup(this.user)
-      .then(
-        () => { 
-            console.log( 'user: ', this.user)
-            this.router.navigate(['/']);
-        })
-      .catch((error: Kinvey.BaseError) => {
-        this.error = error;
-        console.log('signup: error:' + error)
-      });
-      */
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { Kinvey} from  "kinvey-angular2-sdk";
+import { Kinvey, KinveyError} from  "kinvey-angular2-sdk";
 
 if (environment.production) {
   enableProdMode();
@@ -15,10 +15,12 @@ const config:Kinvey.ClientConfig = {
   appSecret: '5d3b5579a09042c79ec8bf86b9b90e99'
 };
 
+console.log( 'main.ts: initialize Kinvey')
 // Initialize
 Kinvey.initialize(config)
-  .then( () => {
+  .then( (a) => {
+    console.log( 'Kinvey.initialize then: ')
     enableProdMode() ;
     platformBrowserDynamic().bootstrapModule(AppModule)
   })
-  .catch(err => console.error(err));
+  .catch((err:string) => console.error( 'error Kinvey.initialize:', err));
